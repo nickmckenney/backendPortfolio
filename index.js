d6522d632a6971e82a6f01deaa13c2f9e479ec9a
nickmckenney <nick.mckenney@fool.com>
2020-09-25 18:42:55 -0400

    testing heroku with basic index.js

--- index.js
+++ index.js
@@ -1,61 +1,16 @@
 
 const express = require('express');
 
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const sendGrid = require('@sendGrid/mail');
+const importData=require("./data.json");
 
 
 const app = express();
 
-
-app.use(bodyParser.json());
-
-app.use(cors());
-
-app.use((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+app.get("/",(req,res)=>{
+    res.send("hi");
 });
-
-app.get('/api', (req, res) => {
-    res.send('API is Working!')
+app.get("/players",(req,res)=>{
+    res.send(importData);
 });
-
-
-app.post('/api/email', (req, res) => {
-
-    console.log(req.body);
-
-    sendGrid.setApiKey('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');
-    const msg = {
-        to: 'mckenney2001@gmail.com',
-        from: 'mckenney2001@gmail.com',//SendGrid only allows to send emails to yourself
-        subject: req.body.email,
-        text: req.body.message
-    }
-
-    sendGrid.send(msg)
-    .then(result => {
-
-        res.status(200).json({
-            success: true
-        });
-
-    })
-    .catch(err => {
-
-        console.log('error: ', err);
-        res.status(401).json({
-            success: false
-        });
-
-    });
-});
-// let port = process.env.PORT || 3000;
-
 app.listen(4000, '0.0.0.0');
 
